6da88c882fad6efa379fc3d1238a90d5c5257c47
wjthompso <49210425+wjthompso@users.noreply.github.com>
2021-10-18 10:56:36 -0700

    Asynchronous API calls, BERT sentence similarities, and SQLAlchemy database management

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import requests
+import os
 import random
 from quotes import *
 
@@ -12,6 +12,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # This silenced annoying wa
 
 db = SQLAlchemy(app)
 
+def create_db_if_necessary():
+    filenames = os.listdir()
+    if "app_database.db" not in filenames:
+        db.create_all()
+
+
 #This will be the database of all total quotes
 class Quote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +27,7 @@ class Quote(db.Model):
     user_rating = db.Column(db.Integer, nullable=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     tags = db.Column(db.String(500), nullable=False)
+    quote_similarities = db.Column(db.JSON, nullable = False)
 
     def __repr__(self):
         return '<Quote %r>' % self.id
@@ -38,34 +45,47 @@ class QuoteHistory(db.Model):
     def __repr__(self):
         return '<Quote %r>' % self.id
 
-def add_all_quotes_to_db():
-    #If the database is empty, perform the API calls and enter the data into our database
+# This sentence similarity matrix was calculated using BERT (Bidirectional Encoder Representations from Transformers), state of the art NLP!
+# This model was presented in a October 2018 paper, it's a very new model!
+# Link to paper: https://arxiv.org/abs/1810.04805
+# Link to medium article with Python code to generate a single row in the matrix: https://towardsdatascience.com/bert-for-measuring-text-similarity-eec91c6bf9e1
+sentence_similarity_matrix = pd.read_csv("sentence_similarity_matrix.csv", index_col = "index")
+all_quote_ids = list(sentence_similarity_matrix.columns)
+
+def add_all_quotes_to_db(sentence_similarity_matrix):
     if Quote.query.count() == 0:
         all_quotes = get_all_quotes()
         for quote in all_quotes:
+            quote_id = quote["_id"]
             new_quote = Quote(
-                quote_id = quote["_id"],
+                quote_id = quote_id,
                 author = quote["author"],
                 quote = quote["content"],
                 user_rating = None,
-                tags = ", ".join(quote["tags"])
+                tags = ", ".join(quote["tags"]),
+                quote_similarities = {quote_id: list(sentence_similarity_matrix[quote_id])},
                 )
             db.session.add(new_quote)
             db.session.commit()
     else:
-        pass
+        return None
 
 @app.route("/", methods=['POST', 'GET'])
 def front_page_quote():
     if request.method == "POST":
-        random_int = random.randint(1,95)
-        random_quote_data = Quote.query.get(random.randint(1,95))
+        if Quote.query.count() != 0:
+            db.session.query(QuoteHistory).delete()
+            db.session.commit()
+
+        random_quote_data = Quote.query.get(random.randint(1, Quote.query.count()))
         return render_template('index.html', random_quote_data=random_quote_data)
 
     else:
+        if Quote.query.count() != 0:
+            db.session.query(QuoteHistory).delete()
+            db.session.commit()
 
-        random_quote_data = Quote.query.get(random.randint(1,95))
-        print(random_quote_data)
+        random_quote_data = Quote.query.get(random.randint(1, Quote.query.count()))
 
         return render_template('index.html', random_quote_data=random_quote_data)
 
@@ -74,45 +94,42 @@ def recommended_quote(id, rating):
     if request.method == 'POST':
         return redirect('/')
     else:
-        # try:
-        # We query the user selected quote and convert it to dictionaries to be used by jinja
-        user_selected_quote = Quote.query.filter_by(quote_id = id).first().__dict__
-        all_quotes = [dict(Quote.__dict__) for Quote in Quote.query.all()] 
-        similar_quote = find_similar_quote(user_selected_quote, all_quotes)     
+        try:
+            # We query the user selected quote and convert it to dictionaries to be used by jinja
+            user_selected_quote = Quote.query.filter_by(quote_id = id).first().__dict__
+            all_quotes = [Quote.__dict__ for Quote in Quote.query.all()]
+            similar_quote = find_similar_quote(user_selected_quote, all_quote_ids)     
 
-        user_quote_history_entry = QuoteHistory(
-                                    quote_id = user_selected_quote['quote_id'],
-                                    author = user_selected_quote['author'],
-                                    quote = user_selected_quote['quote'], #No empty tasks
-                                    user_rating = rating,
-                                    time_of_rating = datetime.utcnow(),
-                                    tags = user_selected_quote['tags']
-                                    )
-        db.session.add(user_quote_history_entry)
-        db.session.commit()
+            user_quote_history_entry = QuoteHistory(
+                                        quote_id = user_selected_quote['quote_id'],
+                                        author = user_selected_quote['author'],
+                                        quote = user_selected_quote['quote'], #No empty quotes
+                                        user_rating = rating,
+                                        time_of_rating = datetime.utcnow(),
+                                        tags = user_selected_quote['tags']
+                                        )
 
-        # get_time_of_rating = datetime.strftime(user_quote_history_entry.time_of_rating, "%Y"
-        print("\n\nThis is what the user_quote_history_entry.time_of_rating object looks like")
-        print(user_quote_history_entry.time_of_rating)
-        print("\n")
+            # Check if the user_quote_history_entry is already in the database, else add and commit
+            if not QuoteHistory.query.filter_by(quote_id = id).first():
+                db.session.add(user_quote_history_entry)
+                db.session.commit()
 
-        # time_of_rating = datetime.strptime(user_quote_history_entry.time_of_rating, '%Y-%m-%d %H:%M:%S.%f').strftime("%I:%M %p (UTC)")
-        time_of_rating = user_quote_history_entry.time_of_rating.strftime("%I:%M %p (UTC)")
+            time_of_rating = user_quote_history_entry.time_of_rating.strftime("%I:%M %p (UTC)")
 
+            all_user_selected_quotes = format_times([user_history.__dict__ for user_history in QuoteHistory.query.all()])
 
-        all_user_selected_quotes = [user_history.__dict__ for user_history in QuoteHistory.query.all()]
-
-        return render_template('recommended_quote.html', user_selected_quote = user_selected_quote,
-                                                        user_rating = rating,
-                                                        time_of_rating = time_of_rating,
-                                                        similar_quote = similar_quote,
-                                                        all_user_selected_quotes = all_user_selected_quotes)
-        # except Exception as e:
-        #     print("There was an issue")
-        #     print(e)
+            return render_template('recommended_quote.html', user_selected_quote = user_selected_quote,
+                                                            user_rating = rating,
+                                                            time_of_rating = time_of_rating,
+                                                            similar_quote = similar_quote,
+                                                            all_user_selected_quotes = all_user_selected_quotes)
+        except Exception as e:
+            print("There was an issue")
+            print(e)
 
 
 if __name__ == '__main__':
-    add_all_quotes_to_db()
+    create_db_if_necessary()
+    add_all_quotes_to_db(sentence_similarity_matrix)
     print(f"\nHere is the count of the database! {Quote.query.count()} \n")
     app.run(host="localhost", port=8002, debug=True)
--- quotes.py
+++ quotes.py
@@ -1,8 +1,10 @@
 import asyncio
 import aiohttp
-import os
 import time
 import json
+import pandas as pd
+from app import QuoteHistory
+from app import Quote
 
 def get_request_tasks(session):
     tasks = []
@@ -39,29 +41,62 @@ def get_all_quotes():
 
     return quotes
 
+# def add_all_quotes
+
 def find_quote_by_id(id):
     all_quotes = get_all_quotes()
     quote_dict = {quote["_id"]: quote for quote in all_quotes}
     return quote_dict[id]
 
+def find_nth_largest_number(nth, unsorted_list):
+    """Find the second largest number means nth = 2
+    Find the third largest number means nth = 3, and so on."""
+    unsorted_list = unsorted_list
+    sorted_list = sorted(list(unsorted_list), reverse = True) # Starts from largest to smallest
+    return unsorted_list.index(sorted_list[nth - 1])
 
-def find_similar_quote(quote, all_quotes):
-    "Takes a flattened list of dictionaries containing quote data"
-    given_quote_tags = set(quote['tags'])
-    for quote_candidate in all_quotes:
-        quote_candidate_tags = set(quote_candidate['tags'])
-        if given_quote_tags.issubset(quote_candidate_tags) and quote['quote_id'] != quote_candidate["quote_id"]:
-            return quote_candidate
-    return None
 
+def find_similar_quote(quote, all_quote_ids):
+    # "Takes a flattened list of dictionaries containing quote data"
+    # print()
+    # print()
+    # print("Here's the quote", quote)
+    # print()
+    # print()
+    quote_similarities = quote["quote_similarities"][quote["quote_id"]]
+    idx_max = find_nth_largest_number(1, quote_similarities) #index of max value, the most similar quote
+    quote_id = all_quote_ids[idx_max] #The quote_id using the index of the maximum similarity score
+    nth = 1
+    while True:
+        if QuoteHistory.query.filter_by(quote_id = quote_id).count() == 0: #Is the quote in our history? No? Carry on
+            return Quote.query.filter_by(quote_id = quote_id).first().__dict__
+        else: #You mean it's already in our quote history? Get another quite, just slightly less similar, but still similar.
+            nth += 1
+            nth_idx_max = find_nth_largest_number(nth, quote_similarities)
+            quote_id = all_quote_ids[nth_idx_max]
+        
+
+# def find_similar_quote(quote, all_quotes):
+#     "Takes a flattened list of dictionaries containing quote data"
+#     given_quote_tags = set(quote['tags'])
+#     for quote_candidate in all_quotes:
+#         quote_candidate_tags = set(quote_candidate['tags'])
+#         if given_quote_tags.issubset(quote_candidate_tags) and quote['quote_id'] != quote_candidate["quote_id"]:
+#             if QuoteHistory.query.filter_by(quote_id = quote_candidate["quote_id"]).count() == 0:
+#                 return quote_candidate
+#     return None
+
+def format_times(all_quotes):
+    for idx, quote in enumerate(all_quotes):
+        all_quotes[idx]["formatted_time"] = quote["time_of_rating"].strftime("%B %-d, %Y at %I:%M %p (UTC)")
+
+    return all_quotes
 
 if __name__ == "__main__":
-    all_quotes = get_all_quotes()
-    print("hey")
-    # single_quote = all_quotes[90]
-    # print()
-    # # print(find_similar_quote(single_quote, all_quotes))
-    # print(find_quote_by_id('nkftU-0YuUP6'))
-    # print()
-    # # print(single_quote)
-    # print("hey")
+    all_quotes = {"all_content": get_all_quotes()}
+    df = pd.read_csv("sentence_similarity_matrix.csv", index_col = "index")
+    
+    with open('all_quotes.json', 'w', encoding ='utf8') as json_file: 
+        json.dump(all_quotes, json_file, ensure_ascii = False) 
+        
+    print("Hey")
